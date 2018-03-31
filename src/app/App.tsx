a0513433e86a3b6c1e868995e5ab13f12e6c2a04
ak33m <akeemtaiwo@outlook.com>
2018-03-31 07:35:01 +0100

    change handlers click handlers

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,12 +2,13 @@ import "./App.scss";
 
 import * as React from "react";
 
-import { Person } from "./person/Person";
+import { Button } from "react-bootstrap";
+import { Person } from "../person/Person";
 
 // tslint:disable:no-var-requires
-const logo = require("./logo.svg");
+const logo = require("../logo.svg");
 
-class App extends React.Component {
+class App extends React.Component<{}, {}> {
 
   state = {
     persons: [
@@ -19,58 +20,76 @@ class App extends React.Component {
     showPersons: false
   };
 
-  deletePersonHandler = (personIndex: number) => {
-    let persons = [...this.state.persons];
+  // deletePersonHandler = (personIndex: number) => {
+  //   let persons = [...this.state.persons];
 
-    persons = this.state.persons.filter((person, index) => {
-      return index !== personIndex;
-    });
+  //   persons = this.state.persons.filter((person, index) => {
+  //     return index !== personIndex;
+  //   });
 
-    this.setState({
-      persons: persons
-    });
-  }
+  //   this.setState({
+  //     persons: persons
+  //   });
+  // }
 
-  nameChangedHandler = (element: HTMLInputElement, id: string) => {
-    const personIndex = this.state.persons.findIndex((p) => {
-      return p.id === id;
-    });
+  nameChangedHandler = (id: string, event: React.FormEvent<HTMLInputElement>) => {
+    // const personIndex = this.state.persons.findIndex((p) => {
+    //   return p.id === id;
+    // });
 
-    let person = { ...this.state.persons[personIndex] };
-    person.name = element.value;
+    // let person = { ...this.state.persons[personIndex] };
+    // person.name = element.value;
 
-    let persons = [...this.state.persons];
-    persons[personIndex] = person;
-
-    this.setState({
-      persons: persons
-    });
-  }
-
-  toggleNameHandler = () => {
-    this.setState({
-      showPersons: !this.state.showPersons
-    });
-  }
-
-  render() {
-    let persons = null;
-    let btnClass: string = "";
-
-    if (this.state.showPersons) {
-      persons =
-        (
-          <div>
-            {
-              this.state.persons.map((person, index) => {
-                return <Person key={person.id} id={person.id} name={person.name} age={person.age} />;
-              })
-            }
-          </div>
-        );
-
-      btnClass = "red";
+    // let persons = [...this.state.persons];
+    // persons[personIndex] = person;
+    let tempState = this.state;
+    if (!!id) {
+      tempState.persons.map((x) => {
+        if (x.id === id) {
+          x.name = event.currentTarget.value;
+        }
+      });
+    } else {
+      tempState.persons.map((x) => {
+        x.name = "Damien";
+      });
     }
+
+    this.setState({
+      persons: tempState.persons
+    });
+  }
+
+  // toggleNameHandler = () => {
+  //   this.setState({
+  //     showPersons: !this.state.showPersons
+  //   });
+  // }
+
+  public render(): React.ReactElement<{}> {
+    let persons = null;
+    // let btnClass: string = "";
+
+    // if (this.state.showPersons) {
+    persons =
+      (
+        <div>
+          {
+            this.state.persons
+              .map((person, index) => {
+                return <Person
+                  key={person.id}
+                  id={person.id}
+                  name={person.name}
+                  age={person.age}
+                  changed={this.nameChangedHandler.bind(this, person.id)}
+                />;
+              })}
+        </div>
+      );
+
+    //   btnClass = "red";
+    // }
 
     let classes = [];
     if (this.state.persons.length <= 2) {
@@ -86,12 +105,12 @@ class App extends React.Component {
           <img src={logo} className="logo" alt="logo" />
           <h1 className="title">Welcome to React</h1>
         </header>
-        <button className={btnClass} onClick={this.toggleNameHandler}>
+        {/* <button className={btnClass} onClick={this.toggleNameHandler}>
           {this.state.showPersons ? "Hide names" : "Show names"}
-        </button>
+         </button> */}
         {persons}
         <p className="intro">
-          {this.state.otherState}
+          <Button onClick={this.nameChangedHandler.bind(this, null)}>Change Name to Damien</Button>
         </p>
       </div>
     );
--- src/person/Person.tsx
+++ src/person/Person.tsx
@@ -4,14 +4,16 @@ interface IPersonProps {
     id: string;
     name: string;
     age: number;
-    click?: () => {};
-    changed?: () => {};
+    click?: () => void;
+    changed?: () => void;
 }
 
 export const Person: React.StatelessComponent<IPersonProps> = (props) => {
     return (
         <div className="person">
-            <p className="display" onClick={props.click}>My name is {props.name}, and I am {props.age} years old.</p>
+            <p className="display" onClick={props.click}>
+                My name is {props.name}, and I am {props.age} years old.
+                </p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name} />
         </div>
